ddb8d848dc129a5de384a76fbf80aa09b9b17d2d
hassankhamis97 <52527199+hassankhamis97@users.noreply.github.com>
2020-06-17 12:41:17 +0200

    project architecture

--- app/src/main/java/com/example/employees/activity/MainActivity.java
+++ app/src/main/java/com/example/employees/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.employees;
+package com.example.employees.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.employees.R;
 
 public class MainActivity extends AppCompatActivity {
 
